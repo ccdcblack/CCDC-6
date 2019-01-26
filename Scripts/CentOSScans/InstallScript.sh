82f6027e4411209578a63f532a7b6b9dd1cbcbd8
dbarr914 <dbarr914@gmail.com>
2019-01-25 21:39:14 -0500

    Update InstallScript.sh

--- Scripts/CentOSScans/InstallScript.sh
+++ Scripts/CentOSScans/InstallScript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-yum clean all
-yum update -y
+sudo yum -y install epel-release
+sudo yum clean all
+sudo yum -y update
 
+yum -y install clamav-server clamav-data clamav-update clamav-filesystem clamav clamav-scanner-systemd clamav-devel clamav-lib clamav-server-systemd lsof redhat-lsb-core nmap wget
 
-sudo yum install -y lsof
-sudo yum install -y redhat-lsb-core
-sudo yum install -y nmap
+wget https://pkg.osquery.io/linux/osquery-3.3.0_1.linux_x86_64.tar.gz
