5ae2e4c022592f69b0387b5c30247e96e77bb4e8
dbarr914 <dbarr914@gmail.com>
2019-01-25 23:46:31 -0500

    Update InstallScript.sh

--- Scripts/CentOSScans/InstallScript.sh
+++ Scripts/CentOSScans/InstallScript.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-sudo yum -y install epel-release
+
 sudo yum clean all
 sudo yum -y update
 
-yum -y install clamav-server clamav-data clamav-update clamav-filesystem clamav clamav-scanner-systemd clamav-devel clamav-lib clamav-server-systemd lsof redhat-lsb-core nmap wget
+yum -y install clamav-server clamav-data clamav-update clamav-filesystem clamav clamav-scanner-systemd clamav-devel clamav-lib clamav-server-systemd lsof redhat-lsb-core nmap wget whowatch yum-utils epel-release
 
-wget https://pkg.osquery.io/linux/osquery-3.3.0_1.linux_x86_64.tar.gz
+yumdownloader lynis
+
+cd /tmp
+wget http://downloads.sourceforge.net/project/rkhunter/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz
+tar -xvf rkhunter-1.4.6.tar.gz 
+cd rkhunter-1.4.6
+sudo ./installer.sh --layout default --install
+/usr/local/bin/rkhunter --update
+/usr/local/bin/rkhunter --propupd
+
+sudo rkhunter --check
+
+
